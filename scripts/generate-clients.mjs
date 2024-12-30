4e61244ff6dc9d8f0f4e4a4b8eeb817e509141a4
Bram Borggreve <beeman@beeman.nl>
2024-12-30 05:02:26 +0000

    chore: generate client

---
+++ scripts/generate-clients.mjs
@@ -0,0 +1,29 @@
+#!/usr/bin/env zx
+import 'zx/globals';
+import * as c from 'codama';
+import { rootNodeFromAnchor } from '@codama/nodes-from-anchor';
+import { renderVisitor as renderJavaScriptVisitor } from '@codama/renderers-js';
+import { getAllProgramIdls } from './utils.mjs';
+
+// Instanciate Codama.
+const [idl, ...additionalIdls] = getAllProgramIdls().map((idl) =>
+  rootNodeFromAnchor(require(idl))
+);
+const codama = c.createFromRoot(idl, additionalIdls);
+
+// Update programs.
+codama.update(
+  c.updateProgramsVisitor({
+    beemanAnchorCounter: { name: 'anchorCounter' },
+  })
+);
+
+
+// Render JavaScript.
+const jsClient = path.join(__dirname, '..', 'clients', 'js');
+codama.accept(
+  renderJavaScriptVisitor(path.join(jsClient, 'src', 'generated'), {
+    prettierOptions: require(path.join(jsClient, '.prettierrc.json')),
+  })
+);
+
